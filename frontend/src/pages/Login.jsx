438006051996144f234213236e0de6cfb736adc8
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-05-22 15:22:01 -0600

    feat/Login: add Basic position and style to the component.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,15 +1,17 @@
 const Login = () => {
   return (
-    <section>
-      <h1>Iniciar sesion</h1>
-      <form>
+    <section className="flex flex-col">
+      <h1 className="text-xl text-black">Iniciar sesion</h1>
+      <form className="flex flex-col">
         <input type="text" placeholder="Mail" />
         <input type="password" placeholder="Contraseña" />
-        <button type="button">Crear cuenta</button>
-        <button type="button">Ingresar</button>
+        <div className="flex">
+          <button type="button">Crear cuenta</button>
+          <button type="button">Ingresar</button>
+        </div>
       </form>
       <p>Olvidaste tu contrasena?</p>
-      <button type="button">Ingresar con FAcebook</button>
+      <button type="button">Ingresar con Facebook</button>
       <button type="button">Ingresar con Google</button>
     </section>
   );
