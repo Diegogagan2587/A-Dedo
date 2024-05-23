b89327117e0a731763fb86301ff77a65e0406397
celinagrcs <celinagrcs@users.noreply.github.com>
2024-05-22 23:07:15 -0300

    feat/main: uptdate

---
+++ frontend/src/pages/Main.jsx
@@ -0,0 +1,14 @@
+import GreenButton from "../components/GreenButton"
+import Navbar from "../components/Navbar"
+
+
+function Main() {
+  return (
+    <div>
+        <Navbar />
+    <GreenButton />
+    </div>
+  )
+}
+
+export default Main
