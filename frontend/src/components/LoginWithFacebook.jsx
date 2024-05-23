d1dd0f18b00e2bab006d5ec788964419150d030c
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-05-22 18:47:44 -0600

    feat/Login(LogintWFacebook btn): Import Facebook Logo

--- frontend/src/components/LoginWithFacebook.jsx
+++ frontend/src/components/LoginWithFacebook.jsx
@@ -1,11 +1,15 @@
+import facebookSVG from '../assets/facebook.svg';
+
 const LoginWithFacebook = () => {
   const handleLogin = () => {
     // Facebook login logic
   };
   return (
-    <button className="bg-blue-500 text-white" onClick={handleLogin}>
-        
-      Ingresar con facebook
+    <button
+     className="bg-blue-500 text-white " 
+    onClick={handleLogin}>
+      <img src={facebookSVG} alt="facebook logo" />
+      <span>Ingresar con facebook</span>
     </button>
   );
 };
