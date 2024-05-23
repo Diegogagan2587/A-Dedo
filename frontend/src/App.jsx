514f672cf132717d15659f7a08054a6f888d1672
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-05-23 00:52:37 -0600

    feat/Router: Install and setup router boiler plate, include routes: / /login and /home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    </>
-  )
+    <Router>
+      <div>
+        <nav></nav>
+        <main>
+          <Routes>
+            <Route path="/" element={<h1>root here</h1>} />
+            <Route path="/login" element={<h1>Login here</h1>} />
+            <Route path="/home" element={<h1>Home here</h1>} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
