6dbcff942866347962374e76f890c7ebe303eec4
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-05-22 18:53:35 -0600

    feat/LoginFBBtn: Add color, center items and round corners

--- frontend/src/components/LoginWithFacebook.jsx
+++ frontend/src/components/LoginWithFacebook.jsx
@@ -6,7 +6,7 @@ const LoginWithFacebook = () => {
   };
   return (
     <button
-     className="bg-blue-500 text-white " 
+     className="bg-[#1877F2] text-white flex items-center justify-center p-2 rounded-full gap-2" 
     onClick={handleLogin}>
       <img src={facebookSVG} alt="facebook logo" />
       <span>Ingresar con facebook</span>
