9583fe8019d7a91192129753eb8740ecc77502d0
Agustin Kowalczuk <Kowalczukagustin@gmail.com>
2024-05-23 03:34:30 -0300

    Add Backend and main structure and folders

---
+++ backend/src/models/trip.model/trip.model.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+const tripSchema = new mongoose.Schema({
+    
+});
+
+export default mongoose.model("Trip", tripSchema);
---
+++ backend/src/router/trip/trip.router.js
@@ -0,0 +1,3 @@
+import {Router} from "express"
+
+export const trip_router = Router();
