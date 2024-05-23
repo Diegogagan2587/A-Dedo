52bec19e61b18fd040af8d3f5ede3bdd120d9049
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-05-23 09:31:52 -0600

    feat/Login: Includes onclick prop in ButtonGreen.jsx

--- frontend/src/components/ButtonGreen.jsx
+++ frontend/src/components/ButtonGreen.jsx
@@ -1,8 +1,9 @@
 import RoundedBtn from "./RoundedBtn";
 
-const ButtonGreen = ({ text, className }) => {
+const ButtonGreen = ({ text, className, onClick }) => {
     return (
         <RoundedBtn
+        onClick={onClick}
         text={text}
         className={`${className} text-[#00A66A] hover:text-white hover:bg-[#00A66A]`}
         />
