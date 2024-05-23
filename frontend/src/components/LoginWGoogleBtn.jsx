980996de7c91da017d28ace6156aa79df81266ff
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-05-22 20:29:46 -0600

    feat/Login: Create Btn for login with google

---
+++ frontend/src/components/LoginWGoogleBtn.jsx
@@ -0,0 +1,19 @@
+import googleSVG from '../assets/google.svg';
+import RoundedBtn from './RoundedBtn';
+
+const LoginWGoogleBtn = ({text}) => {
+  const handleLogin = () => {
+    // Google login logic
+  };
+  return (
+    <RoundedBtn
+        text={text}
+        onClick={handleLogin}
+        className="bg-[#E6E6E6] text-[#49454F]"
+        logo={googleSVG}
+        alt="google logo"
+    />
+  );
+};
+
+export default LoginWGoogleBtn;
