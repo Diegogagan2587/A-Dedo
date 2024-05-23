18877a2a81b883650af2ed8594348802f716e723
Agustin Kowalczuk <Kowalczukagustin@gmail.com>
2024-05-23 15:32:09 -0300

    add Schema/model of User

--- backend/src/models/user.model/user.model.js
+++ backend/src/models/user.model/user.model.js
@@ -1,7 +1,40 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    
+    name: {
+        Type: String,
+        require: true
+    },
+    surname: {
+        Type: String,
+        require: true
+    },
+    password: {
+        Type: String,
+        require: true
+    },
+    rol: [
+        {
+            Type: String,
+            require: true
+        }
+    ],
+    phone: {
+        Type: String,
+        require: true
+    },
+    history_trip:[
+        {
+            Type: mongoose.Schema.Types.ObjectId,
+            ref: "Trip"
+        }
+    ],
+    messages:[
+        {
+            Type: mongoose.Schema.Types.ObjectId,
+            ref: "Message"
+        }
+    ]
 });
 
 export default mongoose.model("User", userSchema);
