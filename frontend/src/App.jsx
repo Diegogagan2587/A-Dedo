5e69ebab5c524f6a6c30cdcfdd421f27c8dfeb06
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-05-23 00:57:00 -0600

    feat/app: Import and add Navbar component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <div>
-        <nav></nav>
+        <Navbar/>
         <main>
           <Routes>
             <Route path="/" element={<h1>root here</h1>} />
