53f013557ace9474e5ee5b23756789866ee0c8b5
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-05-22 23:55:42 -0600

    feat/Login: RoundedBtn.jsx renders an Icon If exist only

--- frontend/src/components/RoundedBtn.jsx
+++ frontend/src/components/RoundedBtn.jsx
@@ -4,7 +4,7 @@ const RoundedBtn = ({ text, onClick, className, logo, alt }) => {
       className={`${className} flex items-center justify-center p-2 rounded-full gap-2`}
       onClick={onClick}
     >
-      <img src={logo} alt={alt} />
+      {logo && <img src={logo} alt={alt} />}
       <span>{text}</span>
     </button>
   );
