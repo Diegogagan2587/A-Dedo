99f472cd6e55e91088ea8cf859d8d79b49009985
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-05-23 09:45:48 -0600

    feat/Login: Implement style for imputs, add onClickHandler place holder

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,23 +1,29 @@
 import LoginWithFacebook from '../components/LoginWithFacebook';
 import LoginWGoogleBtn from '../components/LoginWGoogleBtn';
 import ButtonGreen from '../components/ButtonGreen';
+import Input from '../components/Input';
+
+const handleClick = (e) => {
+  //place holder to prevent default
+  e.preventDefault();
+}
 
 const Login = () => {
   return (
-    <section className="flex flex-col border-2 w-screen h-screen m-0 gap-2 items-center justify-center">
-      <h1 className="text-xl text-black">Iniciar sesion</h1>
-      <form className="flex flex-col gap-2 items-center">
-        <input type="text" placeholder="Mail" />
-        <input type="password" placeholder="Contraseña" />
-        <div className="flex gap-2 items-center">
-          <ButtonGreen text={'Crear Cuenta'} className={"text-lg"}/>
-          <ButtonGreen text={'Ingresar'} className={"text-sm"}/>
+    <section className="flex flex-col w-screen h-screen m-0 items-center gap-4 pl-[48px] pr-[40px] pt-0">
+      <h1 className="my-2 text-xl text-black">Iniciar sesion</h1>
+      <form className="flex flex-col w-full items-center justify-around">
+        <Input type="text" placeholder="Mail" />
+        <Input type="password" placeholder="Contraseña" />
+        <div className="flex gap-2 items-center my-2 w-full">
+          <ButtonGreen onClick={handleClick} text={'Crear Cuenta'} className={"text-lg w-1/2"}/>
+          <ButtonGreen onClick={handleClick} text={'Ingresar'} className={"text-sm w-1/2"}/>
         </div>
       </form>
-      <div className='flex flex-col gap-2'>
+      <div className='flex flex-col gap-2 w-full items-center my-2'>
         <p className='text-sm'>Olvidaste tu contrasena?</p>
-        <LoginWithFacebook text={'Ingresar con facebook'} />
-        <LoginWGoogleBtn text={'Ingresar con Google'} />
+        <LoginWithFacebook onClick={handleClick} text={'Ingresar con facebook'} />
+        <LoginWGoogleBtn onClick={handleClick} text={'Ingresar con Google'} />
       </div>
     </section>
   );
