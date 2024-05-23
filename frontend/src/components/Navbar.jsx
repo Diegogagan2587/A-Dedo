ee05c45e6c6c91736e413a617bfb477bc674d550
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-05-23 15:28:58 -0600

    fix/Navbar: Curve now displays properly

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import logo from '../assets/logo.png';
+import bgNavCurve from '../assets/nav-background.svg';
 
 const Navbar = () => {
   return (
     <nav
-      className={`flex flex-col bg-nav-curve bg-cover bg-no-repeat min-h-[137.5px] p-4 bg-center'`}
+      className="relative"
     >
-      <div className="flex items-center justify-center">
+      <img src={bgNavCurve} alt="background" className="z-0  absolute w-full" />
+      <div className="flex items-center justify-center z-10 relative w-full">
         <div className="flex flex-col items-center">
           <h1 className="font-title text-white text-4xl">a dedo</h1>
           <p className="text-white text-sm text-center ml-8">
