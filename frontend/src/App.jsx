a0358b951ad8aeedac8f3e8dfa21d4caeabdddd5
celinagrcs <celinagrcs@users.noreply.github.com>
2024-05-24 15:50:26 -0300

    feat/home: add the home component to the route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Main from './pages/Main';
 import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/home" element={<h1>Home here</h1>} />
+            <Route path="/home" element={<Home />} />
           </Routes>
         </main>
       </div>
