00727ebaca6ecd03410325041afcc7a5f514189d
celinagrcs <celinagrcs@users.noreply.github.com>
2024-05-24 19:57:52 -0300

    feat/DiverButton: create DriverButton

---
+++ frontend/src/components/DriverButton.jsx
@@ -0,0 +1,11 @@
+
+
+const DriverButton = () => {
+  return (
+    <button className="w-[163px] h-[45px] text-sm border border-customGreen text-customGreen bg-transparent rounded-full m-6">
+        <a href="">Quiero ser conductor</a>
+    </button>
+  )
+}
+
+export default DriverButton
