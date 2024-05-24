9dc38afe4aaa75754dfa0f76e1b5f7fa7fc0855d
celinagrcs <celinagrcs@users.noreply.github.com>
2024-05-24 19:56:53 -0300

    feat/main: fix font-weight

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -4,7 +4,7 @@ function Main() {
   return (
     <div>
         <main className="h-screen bg-main-image flex items-center flex-col justify-center ">
-              <h1 className="text-white font-mainTitle text-center text-5xl font-bold mb-4">Viaja <br /> ahorrando</h1>
+              <h1 className="text-white font-mainTitle text-center text-5xl font-extrabold mb-4">Viaja <br /> ahorrando</h1>
               <p className="text-white w-1/2 text-1xl mb-4 ">Conectate con personas que van al mismo lugar que vos y ahorra dinero.</p>
               <GreenButton />
         </main>
