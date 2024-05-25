69975c531cc8b616134145f5d0de2a89dae6415a
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-05-24 23:07:08 -0600

    feat/app: set up isLogged state  for demo purposes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import Navbar from './components/navbar/Navbar';
 import Main from './pages/Main';
 import Login from './pages/Login';
 
 function App() {
+  //trigger render when user logs in for demo purposes
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <Router>
       <div>
-        <Navbar/>
+        <Navbar isLoggedIn={isLoggedIn} />
         <main>
           <Routes>
             <Route path="/" element={<Main />} />
-            <Route path="/login" element={<Login />} />
+            <Route
+              path="/login"
+              element={<Login setIsLoggedIn={setIsLoggedIn} />}
+            />
             <Route path="/home" element={<h1>Home here</h1>} />
           </Routes>
         </main>
