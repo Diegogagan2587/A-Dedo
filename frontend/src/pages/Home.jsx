a4814ef0275d355e38b3d2c7d1218d55c1d3f132
celinagrcs <celinagrcs@users.noreply.github.com>
2024-05-25 12:16:07 -0300

    feat/home: home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,20 @@
-import CardTrip from "../components/CardTrip"
+// import CardTrip from "../components/CardTrip"
 
+import DriverButton from "../components/DriverButton";
+import mainProfilePicture from '../assets/main-profilepic.png'
 
 const Home = () => {
   return (
-    <div>
-        
+    <main className="mt-20">
+        <section className="flex flex-row items-center justify-between">
+          <img src={mainProfilePicture} alt="" />
+          <div>
+            <h1>Hola Marcos</h1>
+            <DriverButton />
+          </div>
+        </section>
         <section>
-            <CardTrip 
+            {/* <CardTrip 
             profilePicture="src\assets\profile1.png"
             name="Elena"
             seatsAvailable="3"
@@ -14,11 +22,11 @@ const Home = () => {
             startTime="13:00hs"
             endLocation="Mar del Plata"
             endTime="14:00hs"
-            />
-    </section>
+            /> */}
+        </section>
 
-  </div>
-  )
+    </main>
+    )
 }
 
 export default Home
