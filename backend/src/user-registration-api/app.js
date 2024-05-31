97be3c7428fd1341c248fa07e47cdade4f5d2c91
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-05-31 16:02:33 -0600

    fix/back-authenticaton: solve ReferenceError: __dirname is not defined in ES module scope

--- backend/src/user-registration-api/app.js
+++ backend/src/user-registration-api/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
@@ -12,6 +13,9 @@ const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
