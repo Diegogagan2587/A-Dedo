433b8ba6c4c30572c1bf0d517bd14b8bc5626640
celinagrcs <celinagrcs@users.noreply.github.com>
2024-05-31 14:54:39 -0300

    feat/driver-register: dirver register pages

---
+++ frontend/src/pages/driver-register/DriverEnd.jsx
@@ -0,0 +1,18 @@
+import tarjetaIdentificacionLista from '../../assets/documento.png'
+import { NextBtn } from '../../components/NextBtn'
+
+const DriverEnd = () => {
+  return (
+    <main className='mt-20 flex flex-col items-center justify-center'>
+      <img src={tarjetaIdentificacionLista} alt="" />
+      <h2 className='text-[26px] text-mainTitle text-textColor font-extrabold m-3'>¡Felicitaciones!</h2>
+      <p className='text-center m-4 text-[20px]'>Tu documentación fue <br /> validada con exíto.</p>
+
+      <NextBtn
+      href='CarRegister'
+      />
+    </main>
+  )
+}
+
+export default DriverEnd
