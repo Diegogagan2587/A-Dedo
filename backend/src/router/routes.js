bce5a6d0759cf7cf19c0fbcc4203a95dc4d6b6ed
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-05-31 20:18:35 -0600

    feat/routes: rename root_index var to routes to improve readability

--- backend/src/router/routes.js
+++ backend/src/router/routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { allPath } from '../controllers/errors/errorsController.js';
 
-export const root_index = Router();
+export const routes = Router();
 
 // Import Controllers
 import { default as users_controller }  from '../controllers/usersController.js';
@@ -9,14 +9,14 @@ import { default as trips_controller } from '../controllers/tripsController.js'
 import { default as messages_controller } from "../controllers/messagesController.js"
 
 // user
-root_index.use('/user', users_controller);
+routes.use('/user', users_controller);
 
 // trip
-root_index.use('/trip', trips_controller);
+routes.use('/trip', trips_controller);
 
 // message
-root_index.use('/message', messages_controller);
+routes.use('/message', messages_controller);
 
 
 //errors
-root_index.get('*/', allPath);
+routes.get('*/', allPath);
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import express from "express";
 import morgan from "morgan";
 
 //import local
-import { root_index } from "./router/root_index.js";
+import { routes } from "./router/routes.js";
 import { PORT, URI_CONNECTION_DB_LOCAL } from "../config/config.js";
 import { connectMongoDB } from "./db/connection.js";
 
@@ -14,7 +14,7 @@ server.use(morgan("dev"));
 server.use(express.json());
 server.use(express.urlencoded({extended:true}));
 //router
-server.use(root_index);
+server.use(routes);
 
 //connects ORM with DB
 connectMongoDB( URI_CONNECTION_DB_LOCAL || 'mongodb://localhost:27017/adedo');
