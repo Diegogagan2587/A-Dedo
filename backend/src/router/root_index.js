ff118fb3c6733a9cf4c8453320e099e29cbf8fff
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-05-31 19:59:30 -0600

    feat/routes: remove un-necesary importings and connect with controlles

--- backend/src/router/root_index.js
+++ backend/src/router/root_index.js
@@ -1,20 +1,22 @@
-import { Router } from "express";
-import { trip_router } from "./trip/trip.router.js";
-import { user_router } from "./users/users.router.js";
-import { message_router } from "./message/message.router.js";
-import { allPath } from "../controller/errors/error.router.js";
+import { Router } from 'express';
+import { allPath } from '../controllers/errors/errorsController.js';
 
 export const root_index = Router();
 
-
-// message
-root_index.use('/message', message_router);
+// Import Controllers
+import { default as users_controller }  from '../controllers/usersController.js';
+import { default as trips_controller } from '../controllers/tripsController.js'
+import { default as messages_controller } from "../controllers/messagesController.js"
 
 // user
-root_index.use('/user', user_router);
+root_index.use('/user', users_controller);
 
 // trip
-root_index.use('/trip', trip_router);
+root_index.use('/trip', trips_controller);
+
+// message
+root_index.use('/message', messages_controller);
+
 
 //errors
 root_index.get('*/', allPath);
