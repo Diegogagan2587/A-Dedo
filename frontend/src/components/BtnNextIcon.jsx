292f6f8e9f664d482892e43515ba2ccc941c9cea
celinagrcs <celinagrcs@users.noreply.github.com>
2024-06-02 19:02:55 -0300

    feat/create-trip: add btnnexticon

---
+++ frontend/src/components/BtnNextIcon.jsx
@@ -0,0 +1,13 @@
+import back from '../assets/back.png'
+
+const BtnNextIcon = () => {
+  return (
+   <button className='bg-customGreen rounded-full'>
+    <a href="">
+        <img src={back} alt="" className='w-[60px] h-[60px]' />
+    </a>
+   </button>
+  )
+}
+
+export default BtnNextIcon
