2bf6e0c9088b9a31b0da6445843a56d3ce62539d
celinagrcs <celinagrcs@users.noreply.github.com>
2024-06-03 18:48:06 -0300

    feat/create-trip: tripNavbar

--- frontend/src/components/CreateTripNav.jsx
+++ frontend/src/components/CreateTripNav.jsx
@@ -1,11 +1,15 @@
-
+import navbg from "../assets/nav-background.svg"
+import arrow from '../assets/leading-icon.png'
 
 function CreateTripNav() {
   return (
-    <nav>
-
+    <nav >
+      <img src={navbg} alt="background" className=" w-full" />
+      <div className="flex items-center z-10 relative top-0 w-full ">
+        <a href=""><img src={arrow} alt="arrow" /></a> 
+        <h2 className="text-white text-[20px] py-6">Crear viaje</h2>
+      </div>
     </nav>
-
   )
 }
 
