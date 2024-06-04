0323c9c39c8a8d186c9b89512bea45aacd6f4476
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-06-04 12:18:15 -0600

    backend/authenticate: First version of authenticate endpont

--- backend/src/user-registration-api/app.js
+++ backend/src/user-registration-api/app.js
@@ -27,51 +27,7 @@ const mongo_uri = URI_CONNECTION_DB_LOCAL || URI_CONNECTION_DB_ATLAS;
 connectMongoDB(mongo_uri)
 
 
-app.post('/authenticate', async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-
-
-    try {
-
-
-        const loguerUser = User.findOne({ email: email });
-
-        if (loguerUser.isCorrectPassword(password, (err, loguerUser) => {
-            if (err) {
-                res.status(500).send('ERROR AL AUTENTICAR');
-
-            } else if (loguerUser) {
-                res.status(200).send('USUARIO AUTENTICADO CORRECTAMENTE');
-
-            } else {
-                res.status(500).send('USUARIO Y/O  CONTRASEÑA INCORRECTA');
-            }
-        })) {
-
-        }
-        /*User.findOne({ email });
-
-        res.status(200).send('UUSUARIO AUTENTICADO CORRECTAMENTE');*/
-
-    } catch (err) {
-
-        /* User.findOne({ email })
-             if (err) {
-                 res.status(500).send('ERROR AL AUTENTICAR USUARIO');
- 
-             } else if (!user) {
-                 res.status(500).send('EL USUARIO NO EXISTE');
- 
-             }*/
-
-
-
-    }
-})
-
-
-/*app.post('/authenticate', (req, res) => {
+app.post('/authenticate', (req, res) => {
     const { email, password } = req.body;
 
     User.findOne({ email }, (err, user) => {
@@ -96,7 +52,7 @@ app.post('/authenticate', async (req, res) => {
         }
     })
 
-});*/
+});
 
 app.listen(port, () => {
     console.log('Server Started');
