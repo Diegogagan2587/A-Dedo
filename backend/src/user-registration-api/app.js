b9f0e7738d420af9e4ae32856d31b6f63d50fd9a
josent19 <josent19@hotmail.com>
2024-06-04 15:15:18 -0300

    backend: changes athenticate

--- backend/src/user-registration-api/app.js
+++ backend/src/user-registration-api/app.js
@@ -26,10 +26,55 @@ const mongo_uri = URI_CONNECTION_DB_LOCAL || URI_CONNECTION_DB_ATLAS;
 
 connectMongoDB(mongo_uri)
 
-app.post('/authenticate', (req, res) => {
-    const { name, surname, email, password, rol, phone} = req.body;
 
-    user.findOne({ email }, (err, user) => {
+app.post('/authenticate', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+
+    try {
+
+
+        const loguerUser = User.findOne({ email: email });
+
+        if (loguerUser.isCorrectPassword(password, (err, loguerUser) => {
+            if (err) {
+                res.status(500).send('ERROR AL AUTENTICAR');
+
+            } else if (loguerUser) {
+                res.status(200).send('USUARIO AUTENTICADO CORRECTAMENTE');
+
+            } else {
+                res.status(500).send('USUARIO Y/O  CONTRASEÑA INCORRECTA');
+            }
+        })) {
+
+        }
+        /*User.findOne({ email });
+
+        res.status(200).send('UUSUARIO AUTENTICADO CORRECTAMENTE');*/
+
+    } catch (err) {
+
+        /* User.findOne({ email })
+             if (err) {
+                 res.status(500).send('ERROR AL AUTENTICAR USUARIO');
+ 
+             } else if (!user) {
+                 res.status(500).send('EL USUARIO NO EXISTE');
+ 
+             }*/
+
+
+
+    }
+})
+
+
+/*app.post('/authenticate', (req, res) => {
+    const { email, password } = req.body;
+
+    User.findOne({ email }, (err, user) => {
         if (err) {
             res.status(500).send('ERROR AL AUTENTICAR USUARIO');
 
@@ -51,7 +96,7 @@ app.post('/authenticate', (req, res) => {
         }
     })
 
-});
+});*/
 
 app.listen(port, () => {
     console.log('Server Started');
