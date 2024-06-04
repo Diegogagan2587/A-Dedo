3e5cb168ab16fcf0531424b891f293d2c977c566
celinagrcs <celinagrcs@users.noreply.github.com>
2024-06-04 16:19:47 -0300

    feat/create-trip: fix spacing problem

--- frontend/src/components/BtnNextIcon.jsx
+++ frontend/src/components/BtnNextIcon.jsx
@@ -2,11 +2,11 @@ import back from '../assets/back.png'
 
 const BtnNextIcon = () => {
   return (
-   <button className='bg-customGreen rounded-full'>
+  <button className='bg-customGreen rounded-full'>
     <a href="">
         <img src={back} alt="" className='w-[60px] h-[60px]' />
     </a>
-   </button>
+  </button>
   )
 }
 
