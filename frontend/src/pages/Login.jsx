0e4f0ff7293db9b80960713e2745b9f2c8c76e48
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-06-05 13:25:59 -0600

    feat/front/authentication: dispatch athenticate user, set state to logged and redirects to /home in user

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
-
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import  authenticateUser  from '../store/requests/authenticateUser';
 import LoginWithFacebook from '../components/LoginWithFacebook';
 import LoginWGoogleBtn from '../components/LoginWGoogleBtn';
 import ButtonGreen from '../components/ButtonGreen';
@@ -8,12 +10,20 @@ import Input from '../components/Input';
 
 const Login = ({setIsLoggedIn}) => {
   const [user, setUser] = useState({email: '', password: ''});
-
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleClick = (e) => {
     //place holder to prevent default
     e.preventDefault();
     setIsLoggedIn(true);
   }
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    dispatch(authenticateUser(user));
+    setIsLoggedIn(true);
+    navigate('/home');
+  };
   return (
     <section className="flex flex-col w-screen h-screen m-0 items-center gap-4 pl-[48px] pr-[40px] pt-[20vw]">
       <h1 className="my-2 text-xl text-black">Iniciar sesion</h1>
@@ -34,7 +44,7 @@ const Login = ({setIsLoggedIn}) => {
         />
         <div className="flex gap-2 items-center my-2 w-full">
           <ButtonGreen onClick={handleClick} text={'Crear Cuenta'} className={"text-lg w-1/2"}/>
-          <ButtonGreen onClick={handleClick} text={'Ingresar'} className={"text-sm w-1/2"}/>
+          <ButtonGreen onClick={handleLogin} text={'Ingresar'} className={"text-sm w-1/2"}/>
         </div>
       </form>
       <div className='flex flex-col gap-2 w-full items-center my-2'>
