6bbcb965a4d24e21c60c99fabd7e8a27c70b4429
celinagrcs <celinagrcs@users.noreply.github.com>
2024-06-05 16:21:36 -0300

    feat/create-trip: update href prop

--- frontend/src/components/BtnNextIcon.jsx
+++ frontend/src/components/BtnNextIcon.jsx
@@ -1,13 +1,18 @@
 import back from '../assets/back.png'
+import { PropTypes } from 'prop-types';
 
-const BtnNextIcon = () => {
+const BtnNextIcon = ({ href }) => {
   return (
   <button className='bg-customGreen rounded-full'>
-    <a href="">
+    <a href={href}>
         <img src={back} alt="" className='w-[60px] h-[60px]' />
     </a>
   </button>
   )
+}
+
+BtnNextIcon.propTypes = {
+  href: PropTypes.string.isRequired
 }
 
 export default BtnNextIcon
