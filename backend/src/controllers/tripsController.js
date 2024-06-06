b4ad2fe1c35d06eb12e8f2c5b36066b61da8ca43
josent19 <josent19@hotmail.com>
2024-06-06 17:52:58 -0300

    backend: I build the trips API and add to the controller

--- backend/src/controllers/tripsController.js
+++ backend/src/controllers/tripsController.js
@@ -1,5 +1,27 @@
-const tripsController = (request, response)=>{
-    response.json({message: "here is the user trips controller"});
-};
+import Trip from '../models/trip.js'
 
+const tripsController = {
+    create: async (req, res) => {
+        const { destination_point, starting_point, description, date_publication, date_start_trip, ocupation, messages_trip } = req.body;
+
+        try {
+            const newTrip = new Trip({
+                destination_point,
+                starting_point,
+                description,
+                date_publication,
+                date_start_trip,
+                ocupation,
+                messages_trip
+            });
+            await newTrip.save();
+            console.log("viaje guradado");
+            res.status(200).json(newTrip);
+
+        } catch (err) {
+            res.status(500).json({ error: 'ERROR AL CREAR VIAJE ' });
+        }
+    }
+
+}
 export default tripsController;
