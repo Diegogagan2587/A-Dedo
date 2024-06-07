eb4f1e0061e856d9ec66e65fdd152cd93d9cff2e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-06-07 12:11:43 -0600

    feat/front/navbar: overllap drop menu over content, and navbar over dropmenu

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -8,11 +8,17 @@ import menuIcon from '../../assets/menu-icon-hamb.svg';
 const Navbar = ({isLoggedIn}) => {
   const [isActive, setIsActive] = useState(false);
   return (
-    <nav className="fixed w-full top-0">
-      <img src={bgNavCurve} alt="background" className="z-20  absolute w-full" />
+    <nav className="fixed z-30 w-full top-0">
+      <img
+        src={bgNavCurve}
+        alt="background"
+        className="z-20 
+        fixed 
+        w-full top-0"
+      />
       <div className="flex items-center justify-center z-30 relative top-0 pt-2 w-full">
         <div className="flex items-center border-white w-3/4 justify-center">
-          <div className='flex flex-col'>
+          <div className="flex flex-col">
             <h1 className="font-title text-white text-4xl">a dedo</h1>
             <p className="text-white text-sm text-center ml-8">
               viajes compartidos
