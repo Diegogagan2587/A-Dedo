9c9247e0ca16821631790df5e8b358e69b7518b4
celinagrcs <celinagrcs@users.noreply.github.com>
2024-06-07 20:33:53 -0300

    feat/profile-driver: add new components in the profile driver page

--- frontend/src/pages/profile-driver/ProfileDriver.jsx
+++ frontend/src/pages/profile-driver/ProfileDriver.jsx
@@ -1,19 +1,33 @@
 import driverProfile from '../../assets/main-profilepic.png'
+import CardDriverTrip from '../../components/CardDriverTrip'
+import DriverDocuments from '../../components/DriverDocuments'
+import LastTripCard from '../../components/LastTripCard'
+import PassengerDriverBtn from '../../components/PassengerDriverBtn'
 
 const ProfileDriver = () => {
   return (
-    <main className='mt-20 flex flex-col'>
+    <main className='mt-10 flex flex-col'>
       <section className="flex flex-row items-center justify-center">
           <img src={driverProfile} alt=""/>
           <div className="m-4">
             <h1 className="text-center text-2xl font-extrabold text-mainTitle text-textColor mb-2">Marcos</h1>
+            <PassengerDriverBtn />
           </div>
         </section>
-
-        <div className="flex flex-row items-center justify-center">
+        <div className="flex flex-row items-center justify-center mt-4">
           <h1 className="text-customGreen font-extrabold text-mainTitle text-xl m-2">Viajes disponibles</h1>
         </div>
-
+        <section>
+          <CardDriverTrip />
+        </section>
+        <section>
+          <h2 className='font-semibold text-[18px] ml-4'>Mis documentos</h2>
+          <DriverDocuments />
+        </section>
+        <section>
+          <h2 className='font-semibold text-[18px] ml-4'>Último viaje realizado</h2>
+          <LastTripCard />
+        </section>
     </main>
   )
 }
