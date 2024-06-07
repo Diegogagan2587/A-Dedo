264805fcd27d89215387639796d14b1fcc736064
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-06-06 19:44:36 -0600

    feat/front/create-trip(popup) dispatch createTrip with trip data

--- frontend/src/components/PopupCreateTrip.jsx
+++ frontend/src/components/PopupCreateTrip.jsx
@@ -1,9 +1,16 @@
 import { PropTypes } from 'prop-types';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import createTrip from '../store/requests/createTrip'
 
 const PopupCreateTrip = ({ show, onClose }) => {
+  const dispatch = useDispatch();
+  const trip = useSelector((state) => state.trips.new);
+  const handleContinue = async () => {
+    dispatch(createTrip(trip));
+  };
   if (!show) return null;
-
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-600 bg-opacity-50">
       <div className="bg-white p-6 rounded-lg shadow-lg text-center">
@@ -14,8 +21,10 @@ const PopupCreateTrip = ({ show, onClose }) => {
           onClick={onClose}
           className="px-4 py-2 text-[#5F5C5C] rounded-md">
             Cancelar
-          </button>
-          <button className="px-4 py-2 text-customGreen rounded-md">
+          </button >
+          <button 
+          onClick={handleContinue}
+            className="px-4 py-2 text-customGreen rounded-md">
             <Link to="/trips/new/step-5"> Continuar</Link>
           </button>
         </div>
