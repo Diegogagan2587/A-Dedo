fcc1f397e345c65afbf95ef140176a354ff89a19
celinagrcs <celinagrcs@users.noreply.github.com>
2024-06-08 11:06:53 -0300

    feat/profile-driver: add conditional render for the cardtripdriver component

--- frontend/src/pages/profile-driver/ProfileDriver.jsx
+++ frontend/src/pages/profile-driver/ProfileDriver.jsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from 'react'
 import driverProfile from '../../assets/main-profilepic.png'
 import CardDriverTrip from '../../components/CardDriverTrip'
 import DriverDocuments from '../../components/DriverDocuments'
 import LastTripCard from '../../components/LastTripCard'
 import PassengerDriverBtn from '../../components/PassengerDriverBtn'
+import { Link, useLocation } from 'react-router-dom'
 
 const ProfileDriver = () => {
+  const [isTripDeleted, setIstripDeleted] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    if(location.state && location.state.tripDeleted) {
+    setIstripDeleted(true);
+    }
+  }, [location]);
+
   return (
     <main className='mt-10 flex flex-col'>
       <section className="flex flex-row items-center justify-center">
@@ -18,7 +29,16 @@ const ProfileDriver = () => {
           <h1 className="text-customGreen font-extrabold text-mainTitle text-xl m-2">Viajes disponibles</h1>
         </div>
         <section>
-          <CardDriverTrip />
+          { isTripDeleted ? (
+              <div className='flex items-center justify-between'>
+                <h2 className='font-semibold text-[18px] ml-4'>Comenza a ahorrar</h2>
+                <button className="w-[100px] h-[45px] bg-customGreen text-white rounded-full m-6 p-2">
+                  <Link to="/trips/new/step-1">Crear viaje</Link>
+                </button>
+              </div>
+            ) : (
+              <CardDriverTrip />
+          )}
         </section>
         <section>
           <h2 className='font-semibold text-[18px] ml-4'>Mis documentos</h2>
