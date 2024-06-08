0ea7439fb724509288168493a543a5206fbc32b8
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-06-07 20:01:21 -0600

    feat/front/App.jsx: fetch all trips from API

--- frontend/src/store/requests/getTrips.js
+++ frontend/src/store/requests/getTrips.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { VITE_API_URL_BASE as API_URL_BASE } from '../../config';
 
-const getTrips = createAsyncThunk('getTrips', async ()=> {
+const getTrips = createAsyncThunk('trips/get', async ()=> {
   const response = await fetch(`${API_URL_BASE}/trips`)
   const data  = await response.json()
   return data;
