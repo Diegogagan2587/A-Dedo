ea5a398a72498e0e09f78ba588cc0a81b837c5a5
celinagrcs <celinagrcs@users.noreply.github.com>
2024-06-08 17:45:40 -0300

    front/fix: add navbar and fix btn

--- frontend/src/pages/trips/new/CreateTripPartThree.jsx
+++ frontend/src/pages/trips/new/CreateTripPartThree.jsx
@@ -5,43 +5,50 @@ import removeCricle from "../../../assets/remove_circle.png"
 import addCricle from "../../../assets/add_circle.png"
 import BtnNextIcon from "../../../components/BtnNextIcon"
 import { setAvailableSeats } from '../../../store/slices/tripsSlice';
+import BackNav from '../../../components/BackNav';
 
 const CreateTripPartThree = () => {
   const dispatch = useDispatch();
   const [seats, setSeats] = useState(0);
   return (
-    <main className="mt-20">
-      <section className="m-4 flex items-center">
-        <img src={volante} alt="" />
-        <h2 className='text-textColor font-extrabold text-mainTitle text-[26px] m-2'>Acompañantes</h2>
-			</section>
-      <section className="m-4 mt-2 py-3">
-      <h2 className="text-[18px] font-semibold">Cantidad de pasajeros</h2>
-				<input 
-        type="number" 
-        placeholder="Pasajeros" 
-        className='border-[1px] border-black px-4 my-2 py-2 rounded-md text-[#49454F] placeholder-[#49454F] placeholder:text-sm text-base'
-        onChange={(e) => setSeats(e.target.value)}
-        />
-      </section>
-      <section className="m-4 py-3">
-        <h2 className="text-[18px] font-semibold">Selecciona el precio por pasajero</h2>
-        <div className="flex items-center justify-around py-5">
-          <img src={removeCricle} alt="" />
-          <span className="font-semibold text-[26px]">$10.000</span>
-          <img src={addCricle} alt="" />
-        </div>
-        <p className="text-center">
-          <em className="text-[14px] py-4">Valor recomendado por persona</em>
-        </p>
-      </section>
-      <span className="absolute right-12 bottom-20">
-        <BtnNextIcon
-        to="/trips/new/step-4"
-        onClick={() => dispatch(setAvailableSeats(seats))}
-        />
-      </span>
-    </main>
+    <div>
+      <BackNav
+      text="Crear viaje"
+      to="/trips/new/step-2"
+      />
+      <main className="mt-20">
+        <section className="m-4 flex items-center">
+          <img src={volante} alt="" />
+          <h2 className='text-textColor font-extrabold text-mainTitle text-[26px] m-2'>Acompañantes</h2>
+        </section>
+        <section className="m-4 mt-2 py-3">
+        <h2 className="text-[18px] font-semibold">Cantidad de pasajeros</h2>
+          <input 
+          type="number" 
+          placeholder="Pasajeros" 
+          className='border-[1px] border-black px-4 my-2 py-2 rounded-md text-[#49454F] placeholder-[#49454F] placeholder:text-sm text-base'
+          onChange={(e) => setSeats(e.target.value)}
+          />
+        </section>
+        <section className="m-4 py-3">
+          <h2 className="text-[18px] font-semibold">Selecciona el precio por pasajero</h2>
+          <div className="flex items-center justify-around py-5">
+            <img src={removeCricle} alt="" />
+            <span className="font-semibold text-[26px]">$10.000</span>
+            <img src={addCricle} alt="" />
+          </div>
+          <p className="text-center">
+            <em className="text-[14px] py-4">Valor recomendado por persona</em>
+          </p>
+        </section>
+        <span className="absolute right-12 bottom-30">
+          <BtnNextIcon
+          to="/trips/new/step-4"
+          onClick={() => dispatch(setAvailableSeats(seats))}
+          />
+        </span>
+      </main>
+    </div>
   )
 }
 
