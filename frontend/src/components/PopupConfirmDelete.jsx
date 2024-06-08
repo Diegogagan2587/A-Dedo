092a227a74fd5a3422fb3c7fcc58b57d5edb7cdc
celinagrcs <celinagrcs@users.noreply.github.com>
2024-06-08 11:05:48 -0300

    feat/profile-driver: update the PopupConfirmDelete

--- frontend/src/components/PopupConfirmDelete.jsx
+++ frontend/src/components/PopupConfirmDelete.jsx
@@ -1,8 +1,18 @@
 import { PropTypes } from 'prop-types';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const PopupConfirmDelete = ({ show }) => {
+  const navigate = useNavigate();
+
   if (!show) return null;
+
+  const goToProfileDriver = () => {
+    navigate("/profile/driver", {state: {tripDeleted: true}});
+  }
+
+  const goToCreateTrip = () => {
+    navigate("/trips/new/step-1"), {state: {tripDeleted: false}}
+  }
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-600 bg-opacity-50">
@@ -10,11 +20,13 @@ const PopupConfirmDelete = ({ show }) => {
         <h2 className="mb-4 text-lg">Tu viaje ha sido <br /> eliminado con exito.</h2>
         <div className="flex justify-around">
           <button 
-          className="px-4 py-2 text-[#5F5C5C] rounded-md">
-            <Link to={"/profile/driver"}>Ir al inicio</Link> 
+          className="px-4 py-2 text-[#5F5C5C] rounded-md"
+          onClick={goToProfileDriver}>
+            Ir a inicio
           </button>
-          <button className="px-4 py-2 text-customGreen rounded-md">
-            <Link to={"/trips/new/step-1"}>Crear viaje</Link>
+          <button className="px-4 py-2 text-customGreen rounded-md"
+          onClick={goToCreateTrip}>
+            Crear viaje
           </button>
         </div>
       </div>
