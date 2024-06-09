d560474bc10ba95029edc6ee7805977341e2517e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-06-08 19:17:43 -0600

    refact/front/profile-driver: removes un-necesary conde, and import from within Home

--- frontend/src/pages/profile-driver/ProfileDriver.jsx
+++ frontend/src/pages/profile-driver/ProfileDriver.jsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from 'react'
-import driverProfile from '../../assets/main-profilepic.png'
 import CardDriverTrip from '../../components/CardDriverTrip'
 import DriverDocuments from '../../components/DriverDocuments'
 import LastTripCard from '../../components/LastTripCard'
-import PassengerDriverBtn from '../../components/PassengerDriverBtn'
 import { Link, useLocation } from 'react-router-dom'
 
 const ProfileDriver = () => {
@@ -17,14 +15,7 @@ const ProfileDriver = () => {
   }, [location]);
 
   return (
-    <main className='mt-10 flex flex-col'>
-      <section className="flex flex-row items-center justify-center">
-          <img src={driverProfile} alt=""/>
-          <div className="m-4">
-            <h1 className="text-center text-2xl font-extrabold text-mainTitle text-textColor mb-2">Marcos</h1>
-            <PassengerDriverBtn />
-          </div>
-        </section>
+    <div className='flex flex-col '>
         <div className="flex flex-row items-center justify-center mt-4">
           <h1 className="text-customGreen font-extrabold text-mainTitle text-xl m-2">Viajes disponibles</h1>
         </div>
@@ -48,7 +39,7 @@ const ProfileDriver = () => {
           <h2 className='font-semibold text-[18px] ml-4'>Último viaje realizado</h2>
           <LastTripCard />
         </section>
-    </main>
+        </div>
   )
 }
 
