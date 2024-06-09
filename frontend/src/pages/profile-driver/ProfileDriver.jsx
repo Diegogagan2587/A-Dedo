7b37d801c3527e2500826ffafb112804a7d4d4e2
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-06-08 20:09:48 -0600

    refact/front/ProfileDriver: removes un-used code

--- frontend/src/pages/profile-driver/ProfileDriver.jsx
+++ frontend/src/pages/profile-driver/ProfileDriver.jsx
@@ -5,31 +5,23 @@ import LastTripCard from '../../components/LastTripCard'
 import { Link, useLocation } from 'react-router-dom'
 
 const ProfileDriver = () => {
-  const [isTripDeleted, setIstripDeleted] = useState(false);
-  const location = useLocation();
-
-  useEffect(() => {
-    if(location.state && location.state.tripDeleted) {
-    setIstripDeleted(true);
-    }
-  }, [location]);
-
+  const [isNoTrips, setIsNoTrips] = useState(true);
+  
   return (
     <div className='flex flex-col '>
         <div className="flex flex-row items-center justify-center mt-4">
           <h1 className="text-customGreen font-extrabold text-mainTitle text-xl m-2">Viajes disponibles</h1>
         </div>
         <section>
-          { isTripDeleted ? (
+          { isNoTrips && (
               <div className='flex items-center justify-between'>
                 <h2 className='font-semibold text-[18px] ml-4'>Comenza a ahorrar</h2>
                 <button className="w-[100px] h-[45px] bg-customGreen text-white rounded-full m-6 p-2">
                   <Link to="/trips/new/step-1">Crear viaje</Link>
                 </button>
               </div>
-            ) : (
-              <CardDriverTrip />
           )}
+        <CardDriverTrip /> 
         </section>
         <section>
           <h2 className='font-semibold text-[18px] ml-4'>Mis documentos</h2>
