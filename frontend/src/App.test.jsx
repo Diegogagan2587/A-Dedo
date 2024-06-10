07f97846f562e48d18dc3ebbba69e8aabf6733ef
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-06-10 17:38:03 -0600

    test/front/app: fix first test

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,12 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { store } from './store/store';
+import store from './store/store';
 import App from './App';
 
 describe('App', () => {
   it('renders App component', () => {
     render(
-      <Provider store={store}>
+      <Provider store={store} >
         <App />
       </Provider>
     );
