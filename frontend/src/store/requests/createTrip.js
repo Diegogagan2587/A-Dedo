a57ec8ec9f199b7b637b99706a66e72687e6af28
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-06-09 23:48:22 -0600

    fix/front/asyncThunks(createTrip): implement handling of rejection with values

--- frontend/src/store/requests/createTrip.js
+++ frontend/src/store/requests/createTrip.js
@@ -1,19 +1,26 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { VITE_API_URL_BASE as API_URL_BASE} from "../../config";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { VITE_API_URL_BASE as API_URL_BASE } from '../../config';
 
 const createTrip = createAsyncThunk(
   'trip/create',
-  async (trip) => {
-    const response = await fetch(`${API_URL_BASE}/trip/create`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(trip)
-    });
+  async (trip, { rejectWithValue }) => {
+    try {
+      const response = await fetch(`${API_URL_BASE}/trip/create`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(trip),
+      });
 
-    const data = await response.json();
-    return data;
+      const data = await response.json();
+      if (!response.ok) {
+        return rejectWithValue(data);
+      }
+      return data;
+    } catch (err) {
+      return rejectWithValue({ error: err });
+    }
   }
 );
 
