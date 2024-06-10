a5ecb975ceb2dd9280cd63fa673b4d7c7d428d5e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-06-10 17:05:44 -0600

    test/front/config: enable imports from Vitest global, to avoid import manually on each file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     environment: 'jsdom',
+    setupFiles: './test/setup.js',
   }
 })
