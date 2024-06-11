c6e56c9715c3a68897c625d650d7f6fc1820a5b5
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-06-10 21:13:16 -0600

    test/front: Add first test and change Navar header to pass test

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -3,6 +3,14 @@ import { Provider } from 'react-redux';
 import store from './store/store';
 import App from './App';
 
+/*
+Priority
+Queries that are done through ByRole ensure that our UI is accessible to everyone
+no matter what mode they use to navigate the webpage
+getByRole('button', {name: /submit/i})
+getByRole('heading', {level: 1})
+*/
+
 describe('App', () => {
   it('renders App component', () => {
     render(
@@ -11,4 +19,14 @@ describe('App', () => {
       </Provider>
     );
   });
+  
+    it('renders Correct Heading', () => {
+        render(
+        <Provider store={store} >
+            <App />
+        </Provider>
+        );
+        expect(screen.getByRole( 'heading',{level:1} ).textContent).toMatch(/Viaja\s*ahorrando/i);
+    });
+
 });
