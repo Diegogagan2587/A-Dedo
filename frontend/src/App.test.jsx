965172ef6015efc5e7ba447a6617acdea784afed
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-06-11 08:06:24 -0600

    fix/front/tests(App.test.jsx): make correction to wrong import

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,5 +1,5 @@
-import { render, screen, expect } from '@testing-library/react';
-import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import App from './App';
