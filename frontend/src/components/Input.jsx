d87f1dc9e57e197c6411f3f713a37e8370cba532
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-06-20 20:19:37 -0600

    fix/front/Input.jsx: fix accesibility issue, no label

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,14 +1,19 @@
 import { PropTypes } from 'prop-types';
 
-const Input = ({ type, placeholder, value, onChange, required=false }) => {
-  return <input 
-  className='border-[1px] border-black px-4 my-2 py-2 rounded-md text-[#49454F] placeholder-[#49454F] placeholder:text-sm text-base'
-  type={type} 
-  value={value}
-  onChange={onChange}
-  placeholder={placeholder}
-  required={required}
-   />;
+const Input = ({ type, placeholder, value, onChange, required = false, ariaLabel }) => {
+  return (
+    <>
+      <input
+        className="border-[1px] border-black px-4 my-2 py-2 rounded-md text-[#49454F] placeholder-[#49454F] placeholder:text-sm text-base"
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        required={required}
+        aria-label={ariaLabel}
+      />
+    </>
+  );
 };
 
 Input.propTypes = {
@@ -16,7 +21,8 @@ Input.propTypes = {
   placeholder: PropTypes.string,
   value: PropTypes.string,
   onChange: PropTypes.func,
-  required: PropTypes.bool
+  required: PropTypes.bool,
+  ariaLabel: PropTypes.string,
 };
 
 export default Input;
