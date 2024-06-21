ebd2b17d5859530580dd8d0f310d4a415e3d7224
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-06-20 20:00:21 -0600

    test/front/exampleTrips.js: Create array of example trips

---
+++ frontend/tests/integration/exampleTrips.js
@@ -0,0 +1,93 @@
+const exampleTrips = [
+    {
+        "origin": {
+            "address": "La ceiba ",
+            "city": "Villahermosa",
+            "postalCode": "86190",
+            "date": "2024-06-09T00:00:00.000Z",
+            "time": "22:44"
+        },
+        "destination": {
+            "address": "agustin iturbide",
+            "city": "villahermosa",
+            "postalCode": "86288",
+            "date": "2024-06-09T00:00:00.000Z",
+            "time": "23:44"
+        },
+        "_id": "6665334ce1e26f4a7f454489",
+        "driver": {
+            "_id": "6664de33ca7f76873b3667e4",
+            "fullName": "Diego Vidal",
+            "email": "diego@mail.com",
+            "password": "$2b$10$RZ8aaeFiUW9hmUYihePBDutiT9oORbJ6ka3DEBxGyfXj4pWgfOFP6",
+            "rol": [
+                "passenger",
+                "driver"
+            ],
+            "phone": "9934325092",
+            "history_trip": [],
+            "messages": [],
+            "calification": [],
+            "__v": 1,
+            "vehicle": "6664de4aca7f76873b3667e7"
+        },
+        "seats": 7,
+        "price": 0,
+        "stops": false,
+        "pets": true,
+        "childrens": true,
+        "acceptFood": false,
+        "notes": "nope",
+        "passengers": [],
+        "messages_trip": [],
+        "calification": [],
+        "__v": 0
+    },
+    {
+        "origin": {
+            "address": "alberti 123",
+            "city": "mar del plata",
+            "postalCode": "2345",
+            "date": "2024-06-10T00:00:00.000Z",
+            "time": "17:00"
+        },
+        "destination": {
+            "address": "falucho",
+            "city": "santa clara",
+            "postalCode": "1233",
+            "date": "2024-06-10T00:00:00.000Z",
+            "time": "19:54"
+        },
+        "_id": "66673e15fa0f7e1d3ee7f754",
+        "driver": {
+            "_id": "66673d75fa0f7e1d3ee7f750",
+            "fullName": "Celina Garces",
+            "email": "celinagarces6@gmail.com",
+            "password": "$2b$10$yt92vg55fPJGakQOkOZbK.xjnDKorvl8gjzKwUzok2ySudzXwn8jO",
+            "rol": [
+                "passenger",
+                "driver",
+                "driver"
+            ],
+            "phone": "1234566",
+            "history_trip": [],
+            "messages": [],
+            "calification": [],
+            "__v": 2,
+            "vehicle": "66673e89fa0f7e1d3ee7f75b"
+        },
+        "seats": 2,
+        "price": 0,
+        "stops": false,
+        "pets": true,
+        "childrens": false,
+        "acceptFood": true,
+        "notes": "",
+        "passengers": [],
+        "messages_trip": [],
+        "calification": [],
+        "__v": 0
+    },
+  ]
+
+  export default exampleTrips;
