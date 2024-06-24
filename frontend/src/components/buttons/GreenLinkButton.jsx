b394c589ed5deda8eb6cd09ad78426fcbbf8149a
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-06-24 16:09:27 -0600

    feat/front/GreenLinkButton: accept onClick func as props

--- frontend/src/components/buttons/GreenLinkButton.jsx
+++ frontend/src/components/buttons/GreenLinkButton.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 
-const GreenLinkButton = ({ to, text }) => {
+const GreenLinkButton = ({ to, text, onClick }) => {
   return (
     <Link
       className="w-[165px] h-14 bg-customGreen text-white rounded-full m-6 p-2 font-roboto-flex
       flex items-center justify-center"
       to={to}
+      onClick={onClick}
     >
       {text}
     </Link>
@@ -16,6 +17,7 @@ const GreenLinkButton = ({ to, text }) => {
 GreenLinkButton.propTypes = {
   to: PropTypes.string,
   text: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default GreenLinkButton;
