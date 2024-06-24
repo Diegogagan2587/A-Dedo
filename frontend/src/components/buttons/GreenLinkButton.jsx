73d12a36a6d1047ead86acae9f688bd59725f231
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-06-23 20:18:11 -0600

    refact/front/GreenLinkButton: change button to link tag to improve accessibility, make reusable by accepting props

--- frontend/src/components/buttons/GreenLinkButton.jsx
+++ frontend/src/components/buttons/GreenLinkButton.jsx
@@ -1,11 +1,21 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
 
-const RegisterBtn = () => {
+const GreenLinkButton = ({ to, text }) => {
   return (
-    <button className="w-[165px] h-14 bg-customGreen text-white rounded-full m-6 p-2 font-roboto-flex">
-        <Link to='/register/driver-dni'>Comenzar el registro</Link>
-    </button>
-  )
-}
+    <Link
+      className="w-[165px] h-14 bg-customGreen text-white rounded-full m-6 p-2 font-roboto-flex
+      flex items-center justify-center"
+      to={to}
+    >
+      {text}
+    </Link>
+  );
+};
 
-export default RegisterBtn
+GreenLinkButton.propTypes = {
+  to: PropTypes.string,
+  text: PropTypes.string,
+};
+
+export default GreenLinkButton;
