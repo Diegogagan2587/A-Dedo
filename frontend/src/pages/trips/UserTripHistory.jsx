353dce66fbda9b7c0365b79f526f2dc28aace286
celinagrcs <celinagrcs@users.noreply.github.com>
2024-06-29 20:09:59 -0300

    front/user-history-page:  add necessary components to UserTripHistory page

--- frontend/src/pages/trips/UserTripHistory.jsx
+++ frontend/src/pages/trips/UserTripHistory.jsx
@@ -1,10 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import PassengerDriverBtn from "../../components/PassengerDriverBtn"
 import { useDispatch } from "react-redux";
 import { setNavigationLayout } from "../../store/slices/navigationSlice";
+import NoTripsAvailable from "../../components/NoTripsAvailable";
+import LastTripCard from "../../components/LastTripCard";
 
 function UserTripHistory() {
+
   const dispatch = useDispatch();
+  const [driverIsActive, setDriverIsActive] = useState(false);
 
   useEffect(()=>{
     dispatch( setNavigationLayout("back-nav-history") )
@@ -12,8 +16,22 @@ function UserTripHistory() {
   },[dispatch]);
 
   return (
-    <main className="mt-20">
-      <PassengerDriverBtn />
+    <main className="flex items-center justify-center flex-col">
+    <PassengerDriverBtn setDriverIsActive={setDriverIsActive} />
+      {driverIsActive ? (
+        <NoTripsAvailable 
+        text="Al momento no tienes ningun viaje realizado como conductor."
+        />
+      ) : (
+        <LastTripCard 
+        rol="Pasajero"
+        date="21/05/2024"
+        time="13:00hs"
+        price="Valor: $890"
+        startLocation="Barranca de los lobos 303, Santa Clara del mar, Prov de Bs. As."
+        endLocation="Av. Colón 1186, Mar del plata, Prov de Bs. As."
+        />
+      )}
     </main>
   )
 }
